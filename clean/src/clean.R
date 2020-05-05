bccbf56ceac4109b1cfc77482ac98f80dcd97386
JR <jessica.morgan.randall@emory.edu>
2020-05-04 23:07:08 -0400

    organized the script into tasks; clean, model, plot, and docs

---
+++ clean/src/clean.R
@@ -0,0 +1,174 @@
+## Cleaning code for                                               ##
+## "Comparison of multiple tests for determination of seroconversion rates to ##
+## the Chlamydia trachomatis antigen Pgp3: a multi-country analysis" ##
+##                                                                 ##
+## Please feel free to share or modify the code as you see fit     ##   
+## but please maintain appropriate accreditation)                  ##
+##                                                                 ##   
+## Jessica Randall                                                 ##
+## github.com/jessmrandall                                         ##
+
+
+# load libraries 
+pacman::p_load("here","readr", "janitor", 
+               "tidyverse", "assertr", "purrr", 
+               "binom")
+
+# specify file structure of inputs and outputs
+
+files <- list(
+  drc1_Ct694 = here("/clean/input/DRC1Ct694.csv"), 
+  drc1_LFA = here("/clean/input/DRC1LFA.csv"),
+  drc1_MBA = here("/clean/input/DRC1MBA.csv"),
+  drc2_Ct694 = here("/clean/input/DRC1Ct694.csv"), 
+  drc2_LFA = here("/clean/input/DRC1LFA.csv"),
+  drc2_MBA = here("/clean/input/DRC1MBA.csv"),
+  
+  togoLFAf_41 = here("/clean/input/TogoLFAfield_40001.csv"), 
+  togoLFAf_42 = here("/clean/input/TogoLFAfield_40002.csv"), 
+  togoLFAg_41 = here("/clean/input/TogoLFAgold_40001.csv"), 
+  togoLFAg_42 = here("/clean/input/TogoLFAgold_40002.csv"), 
+  togoLFAl_41 = here("/clean/input/TogoLFAlatex_40001.csv"), 
+  togoLFAl_42 = here("/clean/input/TogoLFAlatex_40002.csv"), 
+  togoMBAc_41 = here("/clean/input/TogoMBAct694_40001.csv"), 
+  togoMBAc_42 = here("/clean/input/TogoMBAct694_40002.csv"), 
+  togoMBAp_41 =  here("/clean/input/TogoMBAct694_40001.csv"), 
+  togoMBAp_42 = here("/clean/input/TogoMBAct694_40002.csv"),
+  
+  
+  drc1_Ct694_clean = here("/clean/output/DRC1Ct694_clean.csv"),
+  drc1_ct694agespbins = here("/clean/output/DRC1Ct694_agespbin_df.csv"),
+  
+  drc1_LFA_clean = here("/clean/output/DRC1LFA_clean.csv"),
+  
+  
+  
+  drc1_MBA_clean = here("/clean/output/DRC1MBA_clean.csv"),
+  
+  
+  
+  drc2_Ct694_clean = here("/clean/output/DRC1Ct694_clean.csv"),
+  
+  
+  
+  drc2_LFA_clean = here("/clean/output/DRC1LFA_clean.csv"),
+  
+  
+  
+  drc2_MBA_clean = here("/clean/output/DRC1MBA_clean.csv"),
+  
+  
+  
+  
+  togoLFAf_41_clean = here("/clean/output/TogoLFAfield_40001_clean.csv"),
+  
+  
+  
+  togoLFAf_42_clean = here("/clean/output/TogoLFAfield_40002_clean.csv"),
+  
+  
+  
+  togoLFAg_41_clean = here("/clean/output/TogoLFAgold_40001_clean.csv"), 
+  
+  
+  
+  togoLFAg_42_clean = here("/clean/output/TogoLFAgold_40002_clean.csv"), 
+  
+  
+  
+  togoLFAl_41_clean = here("/clean/output/TogoLFAlatex_40001_clean.csv"), 
+  
+  
+  
+  togoLFAl_42_clean = here("/clean/output/TogoLFAlatex_40002_clean.csv"),
+  
+  
+  
+  togoMBAc_41_clean = here("/clean/output/TogoMBAct694_40001_clean.csv"), 
+  
+  
+  
+  togoMBAc_42_clean = here("/clean/output/TogoMBAct694_40002_clean.csv"),
+  
+
+  
+  togoMBAp_41_clean = here("/clean/output/TogoMBAct694_40001_clean.csv"),
+  
+  
+  
+  togoMBAp_42_clean = here("/clean/output/TogoMBAct694_40002_clean.csv")
+)
+
+stopifnot(is_empty(files) != TRUE & length(files) == 32)
+
+## Read in data
+
+# DRC
+
+# DRC1_CT694
+
+drc1_ct694_df <- read_csv(files$drc1_Ct694, col_names = TRUE, na = "NA") %>%
+  clean_names()
+
+drc1_ct694_df %>%
+  verify(ncol(drc1_ct694_df) == 3 & nrow(drc1_ct694_df) == 1496) %>%
+  verify(is.na(drc1_ct694_df) == FALSE) %>%
+  write_excel_csv(files$drc1_Ct694_clean)
+
+# add in age weighting here, look in "fitted_model_output for DRC1" on the 
+# bottom left hand side (~ line 18)
+
+# age bin the data for plotting
+age_bins <- seq(from=0, to=10, by=1)
+age_bins_mid <- seq(from=0.5, to=9.5, by=1) 
+
+N_bins <- length(age_bins) - 1 
+
+# initialize empty dataframe to fill with binomial confidence intervals
+sp_bins <- data.frame(med = numeric(0), 
+                      low_95 = numeric(0), 
+                      high_95 = numeric(0))
+
+# set random seed
+seed = set.seed(22315)
+
+for(i in 1:N_bins)
+{
+  index <- which(drc1_ct694_df$age > age_bins[i] & 
+                    drc1_ct694_df$age <= age_bins[i+1] ) 
+  
+  temp_AB  <- drc1_ct694_df[index,3]
+  
+  sp_bins[i,] <- as.numeric(as.vector(binom.confint(sum(temp_AB), 
+                                                    nrow(temp_AB), 
+                                                    method="wilson", 
+                                                    seed = seed)[1,4:6]))
+}
+
+stopifnot(is_empty(sp_bins) == FALSE)
+stopifnot(nrow(sp_bins) == 9 & ncol(sp_bins) == 3)
+
+#setup data frames for plots and export
+
+sp_bins_df <- sp_bins %>%
+  mutate(age = as.factor(row.names(sp_bins)))
+
+age_bins_mid_df <- as.data.frame(age_bins_mid) %>%
+  filter(age_bins_mid != 9.5) %>%
+  mutate(age = as.factor(sp_bins_df$age))
+
+age_sp_bins_df <- left_join(sp_bins_df, age_bins_mid_df, by = "age") %>%
+  mutate(age = as.numeric(age))
+
+write_excel_csv(age_sp_bins_df, files$drc1_ct694agespbins)
+
+# drc1_Ct694 
+# drc1_LFA 
+# drc1_MBA 
+# drc2_Ct694 
+# drc2_LFA 
+# drc2_MBA 
+
+# Togo
+
+# done
---
+++ plot/plot.R
@@ -0,0 +1,189 @@
+## Graphing code for                                               ##
+## "Comparison of multiple tests for determination of seroconversion rates to ##
+## the Chlamydia trachomatis antigen Pgp3: a multi-country analysis" ##
+##                                                                 ##
+## Please feel free to share or modify the code as you see fit     ##   
+## but please maintain appropriate accreditation)                  ##
+##                                                                 ##   
+## Jessica Randall                                                 ##
+## github.com/jessmrandall                                         ##
+
+# load libraries 
+pacman::p_load("here", "readr", "janitor", 
+               "tidyverse", "assertr", "ggplot2", "ggthemes", "gridExtra")
+
+# specify file structure of inputs and outputs
+# change to the plot/input/ in final runs
+
+files <- list(
+  drc1_ct694agespbins = here("/clean/output/DRC1Ct694_agespbin_df.csv"), 
+  drc1_ct694agespbins_plot1 = here("/clean/output/DRC1Ct694_ageseroprev.png"))
+
+stopifnot(is_empty(files) != TRUE & length(files) == 2)
+#
+
+pd <- position_dodge(0.1) # move them .05 to the left and right
+
+# DRC1_Ct694 plot 1 
+# read in age_sp_bindf
+drc1_ct694_plot1_df <- read_csv(files$drc1_ct694agespbins, 
+                          col_names = TRUE, na = "NA") %>%
+  clean_names()
+
+#plot age sero prev curve with error bars
+(drc_1_ct694_1 <- ggplot(drc1_ct694_plot1_df, aes(age, med, age_bins_mid)) +
+  geom_pointrange(aes(group = age_bins_mid, 
+                      ymin=med-low_95, 
+                      ymax=med+high_95)) +
+  geom_errorbar(aes(ymin=med-low_95, ymax=med+high_95), 
+                colour="black", width=.1, position=pd) +
+  theme_classic() +
+  labs(title = "Seroprevalence Across Age Groups", 
+       subtitle = "DRC_1 Ct694 with 95% Confidence Interval Error Bars") +
+  xlab("Age") +
+  ylab("Seroprevalence") + 
+  ylim(0, 1.0) +
+  scale_x_continuous(breaks = c(1,2,3,4,5,6,7,8,9)))
+
+ggsave(files$drc1_ct694agespbins_plot1, 
+       plot = last_plot(),dpi = 600,limitsize = TRUE)
+
+
+
+
+# drc1_Ct694 
+# drc1_LFA 
+# drc1_MBA 
+# drc2_Ct694 
+# drc2_LFA 
+# drc2_MBA 
+
+
+#########################################################
+## 2.4 Examine MCMC chains
+
+# move to plot task and plot in tidyverse
+# these files come from the model task
+
+par(mfrow=c(3,3))
+
+for(j in 1:N_par)
+{
+  #####################################
+  ## PANEL j: MCMC chain
+  
+  plot(x=1:N_mcmc, y=MCMC_par[,j], 
+       pch=19, col="grey", cex=0.25,
+       xlab="MCMC iteration", ylab=colnames(MCMC_par)[j], 
+       main=paste("MCMC chain: ", colnames(MCMC_par)[j], " (ESS = ", 
+                  round(effectiveSize(MCMC_par[,j]),0), ")",  sep="") )
+}
+
+#####################################
+## PANEL 3: MCMC log-likelihood
+
+plot(x=1:N_mcmc, y=MCMC_par[,3], 
+     pch=19, col="grey", cex=0.25,
+     ylim=quantile(MCMC_par[,3], prob=c(0.01,1)),
+     xlab="MCMC iteration", ylab="log-likelihood", 
+     main="log-likelihood" )
+
+
+
+#########################################################
+## 2.5 Examine posterior distribution
+
+MCMC_burn <- MCMC_par[floor(0.2*nrow(MCMC_par)):(nrow(MCMC_par)-1),]
+
+for(j in 1:N_par)
+{
+  #####################################
+  ## PANEL j: MCMC posterior
+  
+  DEN <- density( MCMC_burn[,j] )
+  
+  QUANT <- quantile( MCMC_burn[,j], prob=c(0.025, 0.5, 0.975) )
+  
+  plot(x=DEN$x, y=DEN$y, type='l',
+       xlim=c(0, max(DEN$x)),
+       xlab=colnames(MCMC_par)[j], ylab="", 
+       main=paste("Posterior profile: ", colnames(MCMC_par)[j], sep="") )
+  
+  
+  low_index  = which(DEN$x<QUANT[1])
+  mid_index  = intersect( which(DEN$x>=QUANT[1]), which(DEN$x<=QUANT[3]) )
+  high_index = which(DEN$x>QUANT[3])
+  
+  polygon( x=c( DEN$x[low_index], rev(DEN$x[low_index]) ),
+           y=c( rep(0,length(low_index)), rev(DEN$y[low_index]) ), 
+           col="pink")
+  
+  polygon( x=c( DEN$x[mid_index], rev(DEN$x[mid_index]) ),
+           y=c( rep(0,length(mid_index)), rev(DEN$y[mid_index]) ), 
+           col="grey")
+  
+  polygon( x=c( DEN$x[high_index], rev(DEN$x[high_index]) ),
+           y=c( rep(0,length(high_index)), rev(DEN$y[high_index]) ), 
+           col="pink")
+  
+  points(x=rep(QUANT[2],2), y=c(0,max(DEN$y)), type='l', lty="dashed", lwd=2)
+}
+
+
+plot.new()
+
+
+#########################################################
+## 2.6 Auto-correlation
+
+for(j in 1:N_par)
+{
+  autocorr.plot( MCMC_par[,j], auto.layout=FALSE, 
+                 main=paste("Auto-correlation: ", colnames(MCMC_par)[j]) )
+}
+
+plot.new()
+
+
+
+paste( "Acceptance rate = ", round(100*MCMC_accept/N_mcmc,2), "%", sep="" )
+
+
+## move to plot task ## 
+###############################################
+## 3.3 Plot data and model prediction
+
+par(mfrow=c(1,1))
+
+
+plot(x=age_bins_mid, y=SP_bins[,1], 
+     pch=15, cex=2,
+     xlim=c(0,10), ylim=c(0,1),
+     xlab="", ylab="", 
+     main=""  )
+
+
+for(i in 1:N_bins)
+{
+  arrows(x0=age_bins_mid[i], y0=SP_bins[i,2], 
+         x1=age_bins_mid[i], y1=SP_bins[i,3], 
+         length=0.03, angle=90, code=3, col="black", lwd=1)	
+}
+
+
+
+points(x=age_seq, y=M1_quant[2,], 
+       type='l', lwd=3, col="blue")
+
+polygon(x=c(age_seq, rev(age_seq)), 
+        y=c( M1_quant[1,], rev(M1_quant[3,]) ),
+        col=rgb(0/256,0/256,256/256,0.2), border=NA)
+
+
+
+points(x=age_seq, y=M1_predict_median, 
+       type='l', lwd=3, col="blue", lty="dashed")
+
+quantile( MCMC_burn[,1], prob=c(0.5, 0.025, 0.975) )
+
+# done 
